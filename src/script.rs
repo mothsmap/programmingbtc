2c3f5fa3bf22e904e01d1efc44d106c67711d2ab
XuXiang <cartosquare@163.com>
2024-01-03 11:47:26 +0800

    add operator + overwride

--- src/script.rs
+++ src/script.rs
@@ -6,6 +6,7 @@ use anyhow::{bail, Result};
 use num::BigInt;
 use std::{
     fmt,
+    ops,
     io::{Read, Seek},
 };
 
@@ -185,6 +186,18 @@ impl fmt::Display for Script {
     }
 }
 
+impl ops::Add<Script> for Script {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        let mut cmds: Vec<Command> = vec![];
+        cmds.append(&mut self.commands.clone());
+        cmds.append(&mut other.commands.clone());
+
+        Self { commands: cmds }
+    }
+}
+
 mod tests {
     use std::io::Cursor;
 
@@ -223,13 +236,13 @@ mod tests {
         let z = "7c076ff316692a3d7eb3c3bb0f8b1488cf72e1afcd929e29307032997a838a3d";
         let sec = decode_hex("04887387e452b8eacc4acfde10d9aaf7f6d9a0f975aabb10d006e4da568744d06c61de6d95231cd89026e286df3b6ae4a894a3378e393e93a0f45b666329a0ae34").unwrap();
         let sig = decode_hex("3045022000eff69ef2b1bd93a66ed5219add4fb51e11a840f404876325a1e8ffe0529a2c022100c7207fee197d27c618aea621406f6bf5ef6fca38681d82b2f06fddbdce6feab601").unwrap();
-        let mut script_pubkey = Script::new(vec![Command::Element(sec), Command::OP(0xac)]);
+        let script_pubkey = Script::new(vec![Command::Element(sec), Command::OP(0xac)]);
         println!("script pubkey: {}", script_pubkey);
-        let mut script_sig = Script::new(vec![Command::Element(sig)]);
+        let script_sig = Script::new(vec![Command::Element(sig)]);
         println!("script sig: {}", script_sig);
-        script_sig.add(&mut script_pubkey);
-        println!("combined script: {}", script_sig);
-        assert!(script_sig.evaluate(bigint_from_hex(z).unwrap()));
+        let combined_script = script_sig + script_pubkey;
+        println!("combined script: {}", combined_script);
+        assert!(combined_script.evaluate(bigint_from_hex(z).unwrap()));
     }
 
     #[test]
